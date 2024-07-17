dcbce0ec1a337c144250ba8509092c7bcca49708
smreetyyadav37 <smreetyyadav@gmail.com>
2024-07-17 19:52:08 +0530

    files added

---
+++ app.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+# from typing import List
+
+   
+
+setup(
+    name='sensor',
+    version='0.0.1',
+    author='Smreety_Yadav',
+    author_email='smreetyyadav@gmail.com',
+    packages=find_packages(),
+)
